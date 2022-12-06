edf87f5de70e9342d9cd96bbb3913cd7be28edf3
OlivierBourdel <112191849+olibourdel@users.noreply.github.com>
2022-12-06 11:08:36 -0500

    Login and signup

--- wwwroot/ImagesManager2022/js/accountApi.js
+++ wwwroot/ImagesManager2022/js/accountApi.js
@@ -1,11 +1,31 @@
-const apiAccountBaseURL = "http://localhost:5000/api/accounts";
+const apiAccountBaseURL = "http://localhost:5000/accounts";
 
 function LOGIN(data, successCallBack, errorCallBack){
     $.ajax({
-        url: apiAccountBaseURL,
+        url: "http://localhost:5000/token",
         type: 'POST',
         contentType: 'application/json',
         data: JSON.stringify(data),
+        success: (data) => { successCallBack(data) },
+        error: function (jqXHR) { errorCallBack(jqXHR.status) }
+    });
+}
+
+function REGISTER(data, successCallBack, errorCallBack){
+    $.ajax({
+        url: apiAccountBaseURL + '/register',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        success: (data) => { successCallBack(data) },
+        error: function (jqXHR) { errorCallBack(jqXHR.status) }
+    });
+}
+
+function VERIFY(data, successCallBack, errorCallBack){
+    $.ajax({
+        url: apiAccountBaseURL + `/verify?id=${data.Id}&code=${data.Code}`,
+        type: 'GET',
         success: (data) => { successCallBack(data) },
         error: function (jqXHR) { errorCallBack(jqXHR.status) }
     });
