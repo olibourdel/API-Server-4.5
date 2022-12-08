617db87255e453fcb42b5f16fd1726fd29f579b9
olibourdel <112191849+olibourdel@users.noreply.github.com>
2022-12-07 22:50:53 -0500

    .

--- wwwroot/ImagesManager2022/js/accountApi.js
+++ wwwroot/ImagesManager2022/js/accountApi.js
@@ -30,3 +30,38 @@ function VERIFY(data, successCallBack, errorCallBack){
         error: function (jqXHR) { errorCallBack(jqXHR.status) }
     });
 }
+
+function LOGOUT(userId, successCallBack, errorCallBack){
+    $.ajax({
+        url: apiAccountBaseURL + `/logout/${userId}`,
+        type: 'GET',
+        success: (data) => { successCallBack(data) },
+        error: function (jqXHR) { errorCallBack(jqXHR.status) }
+    });
+}
+
+function GET_USER(id, successCallback, errorCallback){
+    $.ajax({
+        url: apiAccountBaseURL + `/index/${id}`,
+        type: 'GET',
+        success: (data) => { successCallback(data) },
+        error: function (jqXHR) { errorCallback(jqXHR.status) }
+    });
+}
+
+function MODIFY(modificationInfo, successCallback, errorCallback){
+    console.log(modificationInfo)
+    $.ajax({
+        url: apiAccountBaseURL + `/modify`,
+        type: 'PUT',
+        contentType: 'application/json',
+        data: JSON.stringify(modificationInfo.account),
+        headers:{
+            authorization : modificationInfo.token.Access_token
+        },
+        success: (data) => { successCallback(data) },
+        error: function (jqXHR) { errorCallback(jqXHR.status) }
+    });
+}
+
+
